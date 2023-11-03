01ca4ff62f24327a06fa5ecdcae8f1e7cf2020ab
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-03 13:52:01 +0530

    Changes

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
 
 function TooltipPortal({ children }) {
-    const portal = (
-        <div className="tooltip-portal">
-          {children}
-        </div>
-      );
-    
-      return ReactDOM.createPortal(portal, portalRoot);
+    return portalRoot ? ReactDOM.createPortal(children, portalRoot) : null;
 }
 
 export default TooltipPortal;

544eeb577682fc0d0a65ee32e2f1a52c6724e1e9
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-03 13:40:23 +0530

    Final Commit

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
 
 function TooltipPortal({ children }) {
-
+    const portal = (
+        <div className="tooltip-portal">
+          {children}
+        </div>
+      );
+    
+      return ReactDOM.createPortal(portal, portalRoot);
 }
 
 export default TooltipPortal;
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,22 @@ import DynamicTooltip from '../components/DynamicTooltip';
 export default function Home() {
   const [showTooltip, setShowTooltip] = useState(false);
 
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
   return (
     <div>
-     <span >Hover over this text to see the dynamic tooltip.</span>
+      <span
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        Hover over this text to see the dynamic tooltip.
+      </span>
       {showTooltip && <DynamicTooltip />}
     </div>
   );
